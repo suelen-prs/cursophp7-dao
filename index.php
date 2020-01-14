29650827b0ab6b2da49fd2fd9bd27353c092e373
Suelen Peres <suelen.prs@gmail.com>
2020-01-13 21:25:53 -0300

    inserir e alterar

--- index.php
+++ index.php
@@ -8,8 +8,36 @@ require_once("config.php");
 
 //echo json_encode($usuarios);
 
-$root = new Usuario();
-$root->loadById(3);
-echo $root;
 
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
+
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("su");
+//echo json_encode($search);
+
+//Carrega um usuario usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("jardim", "amarelo");
+//echo $usuario;
+
+//Criando um novo usuário
+/*$aluno = new Usuario("aluno", "@aluno");
+
+$aluno->insert();
+
+echo $aluno;
+*/
+
+$usuario = new Usuario();
+$usuario->loadById(1);
+$usuario->update("professor", "!@kkkk");
+
+echo $usuario;
 ?>
